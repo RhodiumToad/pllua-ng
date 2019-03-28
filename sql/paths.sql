136c7a4665658d365e61c7ca6d41b75a89756bf5
Andrew Gierth <andrew@tao146.riddles.org.uk>
2019-03-28 11:06:48 +0000

    Fix regression test for paths.
    Don't try and actually stat the path returned, since many PG versions
    won't allow that. Just check we got something vaguely path-looking
    back.

--- sql/paths.sql
+++ sql/paths.sql
@@ -8,17 +8,15 @@ create function pg_temp.tmp1(n text) returns text
   language plluau immutable strict
   as $$ return (require "pllua.paths")[n]() $$;
 
--- some of the dirs might not actually exist, so we test
--- only the important ones.
-select s.isdir
+-- some of the dirs might not actually exist, so we test only the
+-- important ones. We can't actually test that the dir exists or what
+-- the contents are, since many pg versions reject pg_stat_file on
+-- absolute paths; so just check that we got some string that looks
+-- like a path.
+select u.n, f.path ~ '^(?:[[:alpha:]]:)?/'
   from unnest(array['bin','lib','libdir','pkglib','share'])
          with ordinality as u(n,ord),
-       pg_temp.tmp1(u.n) f(path),
-       pg_stat_file(f.path) s
+       pg_temp.tmp1(u.n) f(path)
  order by u.ord;
-
--- test that bin/postgres is found (most other dirs don't
--- have filenames we can predict)
-select s.isdir from pg_stat_file(pg_temp.tmp1('bin') || '/postgres') s;
 
 --end
